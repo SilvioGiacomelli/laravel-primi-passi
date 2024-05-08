63d07958b0d39652ee1473d4637d1c8e8f577b57
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-08 17:06:22 +0200

    [Fix link]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
@@ -11,13 +10,13 @@
 <body>
     <h1>{{ $ciao }}</h1>
     <h2>
-        <a href="http://127.0.0.1:8000/header">Click to Header</a>
+        <a href="{{ route('header') }}">Click to Header</a>
     </h2>
     <h2>
-        <a href="http://127.0.0.1:8000/main">Click to Main</a>
+        <a href="{{ route('main') }}">Click to Main</a>
     </h2>
     <h2>
-        <a href="http://127.0.0.1:8000/footer">Click to Footer</a>
+        <a href="{{ route('footer') }}">Click to Footer</a>
     </h2>
 </body>
 
